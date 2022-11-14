a166844db30aee9940c3a1ed04728be68e11e680
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-11-14 18:55:41 -0300

    Create aula1.py

---
+++ Curso.Udemy/CursoAtualizado/aula1.py
@@ -0,0 +1,11 @@
+def abrirDocumento():
+    try:
+        abrir = open('text-txt', 'w')
+        abrir = write('ola', 'w')
+        abrir = 
+
+    except(erro):
+        print('Erro')
+    
+    finally:
+        fechar = closed()
