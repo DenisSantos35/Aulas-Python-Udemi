224ea9d0c6c5f085d5803df4dd8c1039f4154451
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-11-15 18:35:46 -0300

    int, float, boolean

---
+++ Curso.Udemy/CursoAtualizado/aula4/inteFloat.py
@@ -0,0 +1,26 @@
+'''
+Tipos int float
+int ~> Numero inteiro
+O tipo int representa qualquer numero
+positivo e ou negativo . int sem sina -e considerado 
+positivo.
+'''
+print(11, type(11)) #int
+print(-11, type(-11)) #int
+print(0, type(0)) #int
+
+'''
+Float ~> numero com ponto flutuante
+O tipo float representa qualquer numero
+positivo ou negativo com ponto flutuante.
+float sem sinal e considerado positivo.
+'''
+print(11.1, type(11.1))
+print(-11.1, type(-11.41))
+'''
+A função type mostra o valor que opython inferiu o valor.
+'''
+
+print(type(-11.41), type(11))
+
+
---
+++ Curso.Udemy/CursoAtualizado/aula5/boolean.py
@@ -0,0 +1,13 @@
+'''
+Tipo de dado bool (boolean)
+Ao questionar algo em um programa, 
+só existem duas respostas possivieis:
+yes(true),or no(false)
+Existem varios operadores para questionar
+dentre eles o ==, que é um operador logico que questiona 
+se um valor e igual a outro
+
+'''
+
+print(type(10 == 10)) #sim ~> verdadeiro
+print(10 == 11) #nao ~> false
