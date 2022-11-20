00f05d10b793728b7c47f3fbc164ebd28de35ef0
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-11-20 19:06:10 -0300

    Declaracao variavel e exercicio

---
+++ Curso.Udemy/CursoAtualizado/aula7/variaveis.py
@@ -0,0 +1,19 @@
+#variaveis sao usadas para salvar algo na memoria do computador
+#PEP8: inicie variaveis com letras minusculas, pode usar
+#numeros e underline_.
+# o sinal de = (igual) e o operador de atribuicao. ele e usado
+#para atribuir um valor a um nome (variavel).
+#Uso: nome_variavel = expressão.
+'''
+nome_completo = 'Denis Santos'
+soma = 2 + 2
+
+lista = nome_completo.split(' ')
+print(nome_completo, soma)
+'''
+nome =  'Denis'
+idade = 31
+maior_de_idade = idade >= 18
+
+
+print(f'nome: {nome}, idade: {idade}, é maior de idadede? {maior_de_idade}')
