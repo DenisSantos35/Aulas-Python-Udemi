1a95cae16e8472907f8cef2183149cc1ad9ff55d
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-11-25 22:01:36 -0300

    Create operadoresAritimeticos.py

---
+++ Curso.Udemy/CursoAtualizado/aula9/operadoresAritimeticos.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adicao', adicao)
+
+subtracao = 10 - 5
+print('Subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('multiplicacao', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisao', divisao)
+
+divisaoInteira = 10 // 2.2
+print('Divisao Inteira', divisaoInteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciacao', exponenciacao)
+
+modulo = 55 % 2 #resto da divisao
+print('Modulo', modulo)
