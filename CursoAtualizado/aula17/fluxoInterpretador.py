0bec233521ed113648167d5c848b8edfb54eca48
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2022-12-04 21:43:19 -0300

    fluxo e deburguer interpretador paython

---
+++ CursoAtualizado/aula17/fluxoInterpretador.py
@@ -0,0 +1,22 @@
+condicao = False
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if(condicao):
+    print('Codigo do if')
+elif(condicao1):
+    print('condicao1')
+elif(condicao2):
+    print('condicao2')
+elif(condicao3):
+    print('condicao3')
+elif(condicao4):
+    print('condicao4')
+else:
+    print('codigo do else')
+print('codigo global')
+
+if 10 == 10:
+    print('outro if')
