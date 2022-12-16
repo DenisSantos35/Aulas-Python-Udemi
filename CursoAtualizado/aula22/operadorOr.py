17ef8fd6ce16da07e2ac7e8abb898e65f25a3f9c
DenisSantos35 <denisdiogo18@hotmail.com>
2022-12-15 22:36:35 -0300

    Create operadorOr.py

---
+++ CursoAtualizado/aula22/operadorOr.py
@@ -0,0 +1,27 @@
+#Operadors logicos
+#and(e) or(ou) not(nao)
+# or - qualquer condição verdadeira avalia 
+#toda a expressão com verdadeira
+# a expressão inteira será avaliada naquele valor
+#sao consideradas Falsy(que voce ja viu)
+#0 0.0 '' False
+#tambem existe o tpo None que é 
+#usado para representar um nao valor
+'''
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('senha: ')
+
+senha_permitida = '123456'
+#if == true:
+#  executa o codigo
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
+
+'''
+#Avaliação de curto circuito
+print(0 or False or "" or 'abc' or True)
+
+senha = input('Digite uma senha: ') or 'Sem senha'
+print(senha)
