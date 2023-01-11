36430a8bf84c2cb1d092ca88e6588e7a34b30d1e
DenisSantos35 <denisdiogo18@hotmail.com>
2023-01-10 22:15:03 -0300

    Create builtin.py

---
+++ CursoAtualizado/aula33/builtin.py
@@ -0,0 +1,10 @@
+"""
+Imutaveis que vimos: str, int, float, bool
+"""
+
+string = 'denis Santos'
+outra_variavel = {f'{string[:4]}ABC {string[5:]}'}
+print(string[3])
+print(outra_variavel)
+print(string.capitalize())
+print(string.isalpha())
